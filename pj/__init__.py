e6aded1e4f05ee5210a540b26d4fe64de4b14961
Phil Eaton <philneaton95@gmail.com>
2018-05-06 15:01:36 -0400

    Break out lexer stage

--- pj/__init__.py
+++ pj/__init__.py
@@ -6,13 +6,22 @@ JSON_LEFTBRACKET = '{'
 JSON_RIGHTBRACKET = '}'
 JSON_QUOTE = '"'
 
+JSON_WHITESPACE = [' ', '\t', '\b', '\n', '\r']
+JSON_SYNTAX = [JSON_COMMA, JSON_COLON, JSON_LEFTPAREN, JSON_RIGHTPAREN,
+               JSON_LEFTBRACKET, JSON_RIGHTBRACKET]
+
 FALSE_LEN = len('false')
 TRUE_LEN = len('true')
 NULL_LEN = len('null')
 
 
-def parse_string(string):
+def lex_string(string):
     json_string = ''
+
+    if string[0] == JSON_QUOTE:
+        string = string[1:]
+    else:
+        return None, string
 
     for c in string:
         if c == JSON_QUOTE:
@@ -23,57 +32,7 @@ def parse_string(string):
     raise Exception('Expected end-of-string quote')
 
 
-def parse_array(string):
-    json_array = []
-
-    c = string[0]
-    if c == JSON_RIGHTPAREN:
-        return json_array, string[1:]
-
-    while True:
-        json, string = parse(string)
-        json_array.append(json)
-
-        c = string[0]
-        if c == JSON_RIGHTPAREN:
-            return json_array, string[1:]
-        elif c != JSON_COMMA:
-            raise Exception('Expected comma after object in array')
-        else:
-            string = string[1:]
-
-
-def parse_object(string):
-    json_object = {}
-
-    c = string[0]
-    if c == JSON_RIGHTBRACKET:
-        return json_object, string[1:]
-
-    while True:
-        if string[0] != JSON_QUOTE:
-            raise Exception('Expected string key, got: {}'.format(c))
-
-        json_key, string = parse_string(string[1:])
-
-        if string[0] != JSON_COLON:
-            raise Exception('Expected colon after key in object, got: {}'.format(c))
-
-        json_value, string = parse(string[1:])
-
-        json_object[json_key] = json_value
-
-        c = string[0]
-        if c == JSON_RIGHTBRACKET:
-            return json_object, string[1:]
-        elif c != JSON_COMMA:
-            raise Exception('Expected comma after pair in object, got: {}'.format(c))
-
-        string = string[1:]
-        
-
-
-def parse_number(string):
+def lex_number(string):
     json_number = ''
 
     number_characters = [str(d) for d in range(0, 10)] + ['-', 'e', '.']
@@ -86,40 +45,143 @@ def parse_number(string):
 
     rest = string[len(json_number):]
 
+    if not len(json_number):
+        return None, string
+
     if '.' in json_number:
         return float(json_number), rest
 
     return int(json_number), rest
 
 
-def parse(string, is_root=False):
+def lex_bool(string):
     string_len = len(string)
 
-    c = string[0]
-    if is_root and c != JSON_LEFTBRACKET:
-        raise Exception('Root must be an object')
-
-    if c == JSON_QUOTE:
-        return parse_string(string[1:])
-    elif c == JSON_LEFTPAREN:
-        return parse_array(string[1:])
-    elif c == JSON_LEFTBRACKET:
-        return parse_object(string[1:])
-    elif string_len >= TRUE_LEN and \
+    if string_len >= TRUE_LEN and \
          string[:TRUE_LEN] == 'true':
         return True, string[TRUE_LEN:]
     elif string_len >= FALSE_LEN and \
          string[:FALSE_LEN] == 'false':
         return False, string[FALSE_LEN:]
-    elif string_len >= NULL_LEN and \
+
+    return None, string
+
+
+def lex_null(string):
+    string_len = len(string)
+
+    if string_len >= NULL_LEN and \
          string[:NULL_LEN] == 'null':
-        return None, string[NULL_LEN]
+        return True, string[NULL_LEN]
+
+    return None, string
+
+
+def lex(string):
+    tokens = []
+
+    while len(string):
+        json_string, string = lex_string(string)
+        if json_string is not None:
+            tokens.append(json_string)
+            continue
+
+        json_number, string = lex_number(string)
+        if json_number is not None:
+            tokens.append(json_number)
+            continue
+
+        json_bool, string = lex_bool(string)
+        if json_bool is not None:
+            tokens.append(json_bool)
+            continue
+
+        json_null, string = lex_null(string)
+        if json_null is not None:
+            tokens.append(None)
+            continue
+
+        c = string[0]
+
+        if c in JSON_WHITESPACE:
+            # Ignore whitespace
+            string = string[1:]
+        elif c in JSON_SYNTAX:
+            tokens.append(c)
+            string = string[1:]
+        else:
+            raise Exception('Unexpected character: {}'.format(c))
+
+    return tokens
+
+
+def parse_array(tokens):
+    json_array = []
+
+    t = tokens[0]
+    if t == JSON_RIGHTPAREN:
+        return json_array, tokens[1:]
+
+    while True:
+        json, tokens = parse(tokens)
+        json_array.append(json)
+
+        t = tokens[0]
+        if t == JSON_RIGHTPAREN:
+            return json_array, tokens[1:]
+        elif t != JSON_COMMA:
+            raise Exception('Expected comma after object in array')
+        else:
+            tokens = tokens[1:]
+
+
+def parse_object(tokens):
+    json_object = {}
+
+    t = tokens[0]
+    if t == JSON_RIGHTBRACKET:
+        return json_object, tokens[1:]
+
+    while True:
+        json_key = tokens[0]
+        if type(json_key) is str:
+            tokens = tokens[1:]
+        else:
+            raise Exception('Expected string key, got: {}'.format(json_key))
+
+        if tokens[0] != JSON_COLON:
+            raise Exception('Expected colon after key in object, got: {}'.format(t))
+
+        json_value, tokens = parse(tokens[1:])
+
+        json_object[json_key] = json_value
+
+        t = tokens[0]
+        if t == JSON_RIGHTBRACKET:
+            return json_object, tokens[1:]
+        elif t != JSON_COMMA:
+            raise Exception('Expected comma after pair in object, got: {}'.format(t))
+
+        tokens = tokens[1:]
+        
+
+def parse(tokens, is_root=False):
+    t = tokens[0]
+
+    if is_root and t != JSON_LEFTBRACKET:
+        raise Exception('Root must be an object')
+
+    if t == JSON_LEFTPAREN:
+        return parse_array(tokens[1:])
+    elif t == JSON_LEFTBRACKET:
+        return parse_object(tokens[1:])
     else:
-        return parse_number(string)
+        return t, tokens[1:]
 
 
 def from_string(string):
-    return parse(string, is_root=True)[0]
+    tokens = lex(string)
+    return parse(tokens, is_root=True)[0]
 
 
 def to_string(json):
